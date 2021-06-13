5c14e25227355aa6c97d2e8f58f38e7bc3fde188
le Téno <leteno.loic@gmail.com>
2021-06-13 17:51:13 +0200

    commencement media queries LLT

--- public/js/modules/_style.js
+++ public/js/modules/_style.js
@@ -1,6 +1,9 @@
 // MODE SOMBRE
+let titre = document.querySelector('#titre span');
 let nav = document.querySelector('nav');
+let toggle = document.querySelector('#hamburger');
 let nav_a = Array.from(document.querySelectorAll('nav a'));
+console.log(titre);
 
 
     // MÉTHODE
@@ -12,16 +15,21 @@ function lightside(light) {
         light.classList.remove('dark-mode');
 }
 
+
     // ON
 let button_black = document.querySelector('.input_dark');
 button_black.addEventListener('click', () => {
     darkside(document.body);
+
+    titre.classList.add('span-2')
 
     darkside(nav);
 
     nav_a.forEach(e => {
         darkside(e);
     })
+
+    darkside(toggle);
 });
 
     // OFF
@@ -29,11 +37,15 @@ let button_white = document.querySelector('.input_light');
 button_white.addEventListener('click', () => {
     lightside(document.body);
 
+    titre.classList.remove('span-2')
+
     lightside(nav);
 
     nav_a.forEach(e => {
         lightside(e);
     })
+
+    lightside(toggle);
 });
 
 
